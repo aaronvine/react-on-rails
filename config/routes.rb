0bc62bcd0715dfa4948dc1ca39fb7df3f76852d7
Aaron <aaronvine@gmail.com>
2016-11-26 15:32:42 +0200

    progress

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,24 @@
 require 'api_constraints'
 
 Rails.application.routes.draw do
+  namespace :api do
+  namespace :v1 do
+    get 'comments/index'
+    end
+  end
+
+  namespace :api do
+  namespace :v1 do
+    get 'comments/show'
+    end
+  end
+
+  namespace :api do
+  namespace :v1 do
+    get 'comments/create'
+    end
+  end
+
   namespace :api, defaults: { format: :json }, constraints: { subdomain: 'api' }, path: '/' do
     scope module: :v1, constraints: ApiConstraints.new(version: 1, default: true) do
       resources :comments
